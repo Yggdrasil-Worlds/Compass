0dab16cd8b53bfaeec0e574e66ef1453baac48f5
MondGnu <mondgnu@gmail.com>
2024-04-15 22:52:49 +0200

    added some documentation

--- src/main/java/de/yggdrasil/compass/CompassBuilder.java
+++ src/main/java/de/yggdrasil/compass/CompassBuilder.java
@@ -25,19 +25,43 @@ public class CompassBuilder {
         items = Set.of();
     }
 
+    /**
+     * Add item to {@link Compass}
+     * @param item item to add
+     * @param slot where to put
+     */
     public void AddItem(CompassItem item, int slot) {
         items.add(new PositionedCompassItem(item, slot));
     }
+
+    /**
+     * Add item to {@link Compass}
+     * @param item item to add
+     */
     public void AddItem(PositionedCompassItem item) {
         items.add(item);
     }
 
+    /**
+     * Sets title of the {@link Compass}
+     * @param title title to set
+     */
     public void SetTitle(String title) {
         this.title = title;
     }
+
+    /**
+     * Sets size of the compass inventory
+     * @param size size to set
+     */
     public void SetSize(CompassSize size) {
         this.size = size;
     }
+
+    /**
+     * Tries to build a compass
+     * @return  null if no title, size or items
+     */
     public @Nullable Compass Build() {
         if(items.isEmpty() || title == null || size == null)
             return null;
